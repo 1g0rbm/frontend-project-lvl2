d0a9b6d209abd80714e124be9dd4c3aa8701bcfc
1g0rbm <m1g0rb89@gmail.com>
2021-05-31 21:57:27 +0300

    Added flat json parsing

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 import program from 'commander'
+import genDiff from '../index.js'
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2)
+    console.log(diff)
+  });
 
 program.parse()
---
+++ src/genDiff.js
@@ -0,0 +1,61 @@
+import { cwd } from 'process';
+import { readFileSync, existsSync } from 'fs';
+import { join } from 'path';
+import _ from 'lodash';
+
+const normalizePath = (path) => {
+  if (existsSync(path)) {
+    return path;
+  }
+
+  const absolutePath = join(cwd(), path);
+  return existsSync(absolutePath) ? absolutePath : null;
+}
+
+const showError = (text) => console.error(text)
+
+const getFormattedDiff = (diff) => JSON.stringify(diff, null, '\t');
+
+const genDiff = (filepath1, filepath2) => {
+  const normalisedFilepath1 = normalizePath(filepath1)
+  if (!normalisedFilepath1) {
+    showError(`<filepath1> value "${filepath1}" is incorrect.`);
+    return;
+  }
+  const normalisedFilepath2 = normalizePath(filepath2)
+  if (!normalisedFilepath2) {
+    showError(`<filepath2> value "${filepath2}" is incorrect.`);
+    return;
+  }
+
+  const file1 = readFileSync(normalisedFilepath1);
+  const file2 = readFileSync(normalisedFilepath2);
+
+  const data1 = JSON.parse(file1.toString());
+  const data2 = JSON.parse(file2.toString());
+
+  const keys = _.uniq(_.sortBy([...Object.keys(data1), ...Object.keys(data2)]));
+  const diff = keys
+    .reduce((acc, key) => {
+      const val1 = _.get(data1, key)
+      const val2 = _.get(data2, key)
+
+      if (val1 === val2) {
+        return [...acc, [`  ${key}`, val1]]
+      }
+
+      return [
+        ...acc,
+        [`- ${key}`, val1],
+        [`+ ${key}`, val2],
+      ]
+    }, [])
+    .filter(([, val]) => val !== undefined)
+    .reduce((acc, [key, val]) => {
+      return { ...acc, ...{ [key]: val } }
+    }, {});
+
+  return getFormattedDiff(diff);
+}
+
+export default genDiff;
