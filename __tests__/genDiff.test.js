48cebe0a40ee4fc930ff5ef42c9602998efab6c3
1g0rbm <m1g0rb89@gmail.com>
2021-06-02 00:39:20 +0300

    Configured jest

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+test('main flow', () => {
+  const filepath1 = join(__dirname, '__fixtures__', 'file1.json');
+  const filepath2 = join(__dirname, '__fixtures__', 'file2.json');
+  const expected = {
+    '- follow': false,
+    '  host': 'hexlet.io',
+    '- proxy': '123.234.53.22',
+    '- timeout': 50,
+    '+ timeout': 20,
+    '+ verbose': true
+  };
+
+  expect(genDiff(filepath1, filepath2)).toBe(JSON.stringify(expected, null, '\t'))
+})
