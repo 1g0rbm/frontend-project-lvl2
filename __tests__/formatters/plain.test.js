f0b04387570d7ecc64e320d94cfa047f5e2f4c3f
1g0rbm <m1g0rb89@gmail.com>
2021-06-12 11:27:37 +0300

    Several fixes

--- __tests__/formatters/plain.test.js
+++ __tests__/formatters/plain.test.js
@@ -5,23 +5,23 @@ import plain from '../../src/formatters/plain';
 test('plain formatter test', () => {
   const ast = buildDiffAst({
     a: 'AA',
-    b: 'BB',
+    b: 'DD',
     c: {
-      d: 'DD',
+      d: 'NN',
       e: 'EE',
     },
   },
   {
     a: 'AA',
     c: {
-      e: 'DD',
+      e: 'NN',
       f: 'FF',
     },
     g: 'GG',
   });
   const expected = `Property 'b' was removed
 Property 'c.d' was removed
-Property 'c.e' was updated. From 'EE' to 'DD'
+Property 'c.e' was updated. From 'EE' to 'NN'
 Property 'c.f' was added with value: 'FF'
 Property 'g' was added with value: 'GG'`;
 
