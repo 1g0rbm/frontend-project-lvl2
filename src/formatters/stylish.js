f3f006d71c86d2c3fd0bace2bf61c607016363ee
1g0rbm <m1g0rb89@gmail.com>
2021-06-23 20:49:27 +0300

    Fixed level indent defining

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getLvlIndent = (cnt, sign = ' ') => `${'    '.repeat(cnt - 1)}  ${sign} `;
+const getLvlIndent = (repeatCount, sign = ' ') => `${'    '.repeat(repeatCount)}  ${sign} `;
 
 const serialize = (value, depth) => {
   if (!_.isObject(value)) {
@@ -8,24 +8,24 @@ const serialize = (value, depth) => {
   }
 
   const nested = _.keys(value).map(
-    (key) => `${getLvlIndent(depth)}${key}: ${serialize(value[key], depth + 1)}`,
+    (key) => `${getLvlIndent(depth - 1)}${key}: ${serialize(value[key], depth + 1)}`,
   );
 
-  return `{\n${nested.join('\n')}\n${getLvlIndent(depth - 1)}}`;
+  return `{\n${nested.join('\n')}\n${getLvlIndent(depth - 2)}}`;
 };
 
 const format = ({ name, value, type }, lvl) => {
   const typeFormatters = {
-    added: () => `${getLvlIndent(lvl, '+')}${name}: ${serialize(value, lvl + 1)}`,
-    removed: () => `${getLvlIndent(lvl, '-')}${name}: ${serialize(value, lvl + 1)}`,
+    added: () => `${getLvlIndent(lvl - 1, '+')}${name}: ${serialize(value, lvl + 1)}`,
+    removed: () => `${getLvlIndent(lvl - 1, '-')}${name}: ${serialize(value, lvl + 1)}`,
     changed: () => (
       [
-        `${getLvlIndent(lvl, '-')}${name}: ${serialize(value[0], lvl + 1)}`,
-        `${getLvlIndent(lvl, '+')}${name}: ${serialize(value[1], lvl + 1)}`,
+        `${getLvlIndent(lvl - 1, '-')}${name}: ${serialize(value[0], lvl + 1)}`,
+        `${getLvlIndent(lvl - 1, '+')}${name}: ${serialize(value[1], lvl + 1)}`,
       ]
         .join('\n')
     ),
-    unchanged: () => `${getLvlIndent(lvl)}${name}: ${serialize(value, lvl)}`,
+    unchanged: () => `${getLvlIndent(lvl - 1)}${name}: ${serialize(value, lvl)}`,
   };
 
   return _.get(typeFormatters, type)();
@@ -35,7 +35,7 @@ const build = (tree, lvl) => (
   tree.map((node) => {
     if (node.type === 'tree') {
       const treeValue = build(node.children, lvl + 1);
-      return `${getLvlIndent(lvl)}${node.name}: {\n${treeValue}\n${getLvlIndent(lvl)}}`;
+      return `${getLvlIndent(lvl - 1)}${node.name}: {\n${treeValue}\n${getLvlIndent(lvl - 1)}}`;
     }
 
     return format(node, lvl);
